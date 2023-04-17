a0d09efb80792216bcc53cc26a6385dec534bd65
Putnug1122 <asiapatta10@gmail.com>
2023-04-17 12:29:47 +0800

    ui done

--- app/routes/_app.expenses.tsx
+++ app/routes/_app.expenses.tsx
@@ -1,4 +1,5 @@
-import { Outlet } from "@remix-run/react";
+import { Link, Outlet } from "@remix-run/react";
+import { FaDownload, FaPlus } from "react-icons/fa";
 import ExpensesList from "~/components/expenses/ExpensesList";
 
 const dummyData = [
@@ -19,8 +20,18 @@ const dummyData = [
 export default function ExpenseLayout() {
   return (
     <>
+      <Outlet />
       <main>
-        <Outlet />
+        <section id="expenses-actions">
+          <Link to="add">
+            <FaPlus />
+            <span>Add Expense</span>
+          </Link>
+          <a href="/expenses/raw">
+            <FaDownload />
+            <span>Load Raw Data</span>
+          </a>
+        </section>
         <ExpensesList expenses={dummyData} />
       </main>
     </>
