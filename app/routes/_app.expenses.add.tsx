9c2d3e0d9364d1597285bec0eb0213934cbecae9
Putra Nugraha <asiapatta10@gmail.com>
2023-04-18 10:44:10 +0800

    validation and form component

--- app/routes/_app.expenses.add.tsx
+++ app/routes/_app.expenses.add.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "@remix-run/react";
 import ExpenseForm from "~/components/expenses/ExpenseForm";
 import Modal from "~/components/util/Modal";
 import { addExpense } from "~/data/expenses.server";
+import { validateExpenseInput } from "~/data/validation.server";
 
 export default function ExpenseAddPage() {
   const navigate = useNavigate();
@@ -20,6 +21,17 @@ export default function ExpenseAddPage() {
 
 export async function action({ request }: { request: Request }) {
   const formData = await request.formData();
+
+  try {
+    validateExpenseInput({
+      title: formData.get("title") as string,
+      amount: formData.get("amount") as string,
+      date: formData.get("date") as string,
+    });
+  } catch (error) {
+    return error;
+  }
+
   const expense = {
     title: formData.get("title") as string,
     amount: formData.get("amount") as unknown as number,
